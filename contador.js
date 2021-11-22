9d243beaf46dc0432a80a7b6fa372f54c28ac8fe
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2021-11-22 16:33:04 -0300

    agregando javascript

---
+++ contador.js
@@ -0,0 +1,58 @@
+
+function init(){
+   
+   let contador = 0
+
+   let primerBoton=document.getElementById('-1')
+   let segundoBoton=document.getElementById('+1')
+   let tercerBoton=document.getElementById('-5')
+   let cuartoBoton=document.getElementById('+5')
+   let quintoBoton=document.getElementById('-10')
+   let sextoBoton=document.getElementById('+10')
+
+   let contenedor=document.getElementById('contenedor')
+   let creandoH2=document.createElement('h2')
+
+   contenedor.appendChild(creandoH2)
+
+   
+
+
+   primerBoton.onclick= function(e){
+      contador--
+      console.log(contador)
+      creandoH2.innerHTML=contador
+   }
+
+   segundoBoton.onclick=function(e){
+      contador++
+      console.log(contador)
+      creandoH2.innerHTML=contador
+   }
+
+   tercerBoton.onclick=function(e){
+      contador=contador-5
+      console.log(contador)
+      creandoH2.innerHTML=contador
+   }
+   
+   cuartoBoton.onclick=function(e){
+      contador=contador+5
+      console.log(contador)
+      creandoH2.innerHTML=contador
+   }
+
+   quintoBoton.onclick=function(e){
+      contador=contador-10
+      console.log(contador)
+      creandoH2.innerHTML=contador
+   }
+
+   sextoBoton.onclick=function(e){
+      contador=contador+10
+      console.log(contador)
+      creandoH2.innerHTML=contador
+   }
+
+
+}
